2c1ed5cb1f9121bfcdd078b349ad6f06890791c1
chhotuthakur <npsdahiya@gmail.com>
2023-05-19 19:16:30 +0530

    updates 15.6

--- app/src/main/java/com/nilesh/timeline/MainActivity.java
+++ app/src/main/java/com/nilesh/timeline/MainActivity.java
@@ -1,14 +1,22 @@
 package com.nilesh.timeline;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,13 +37,63 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
+                Random random = new Random();
+
                 String tit = title.getText().toString();
                 String con = content.getText().toString();
-                String id = tit;
+
+                String code = String.format("%04d", random.nextInt(1000));
+                String id = tit.substring(0,4)+ code;
+                id = id.replace(" ", "");
 
                 databaseReference.child("upsc").child(id).child("title").setValue(tit);
                 databaseReference.child("upsc").child(id).child("content").setValue(con);
+
+                databaseReference.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        if (snapshot.hasChild("title")){
+                            clearForm();
+                        }
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
+
         });
+
     }
+
+    public  void  clearForm() {
+//
+        ViewGroup group = (ViewGroup) findViewById(R.id.parent);
+        for (int i = 0, count = group.getChildCount(); i < count; ++i) {
+            View view = group.getChildAt(i);
+            if (view instanceof EditText) {
+                ((EditText) view).setText("");
+            }
+        }
+
+
+}
 }
